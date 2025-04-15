14fdaa61d04dde0896e962a14d7d104baccefbf3
lucabro81 <luca.brognara81@gmail.com>
2025-04-15 23:14:18 +0200

    chore: code cleaning, feat: new types

--- packages/core/src/common/types.ts
+++ packages/core/src/common/types.ts
@@ -1,3 +1,17 @@
 export type EntityId = string;
+export type ComponentType = 'description' | 'connections' | 'location' | 'inventory' | 'pickupable' | 'buttonState';
 export type RoomId = string;
 export type Timestamp = number;
+
+export interface IComponent {
+  readonly type: ComponentType;
+}
+
+export type WorldType = Map<EntityId, Map<ComponentType, IComponent>>
+
+export interface DescriptionComponent extends IComponent { type: 'description'; name: string; keywords: string[]; text: string; }
+export interface RoomConnectionsComponent extends IComponent { type: 'connections'; exits: Record<string, EntityId[]>; }
+export interface IsPresentInRoomComponent extends IComponent { type: 'location'; roomId: EntityId; }
+export interface InventoryComponent extends IComponent { type: 'inventory'; items: EntityId[]; } // Per oggetti nella stanza o nell'inventario PG
+export interface IsPickupableComponent extends IComponent { type: 'pickupable'; } // Componente "marker"
+export interface ButtonStateComponent extends IComponent { type: 'buttonState'; isPushed: boolean; }
--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -1,15 +1,3 @@
 /**
  * core - Main entry point
  */
-
-import { hello } from './core';
-
-// Re-export core functionality
-export { hello };
-
-// Export types
-export * from './types';
-
-export default {
-  hello
-}; 
